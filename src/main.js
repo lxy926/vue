5c95c5e6f504871d415f7490b1c14926b9e2cea7
lxy926 <1073008504@qq.com>
2019-10-21 22:59:22 +0800

    项目第一天

--- src/main.js
+++ src/main.js
@@ -1,14 +1,33 @@
 //入口文件
 import Vue from 'vue'
 
+//1.1导入路由包
+import VueRouter from 'vue-router'
+//1.2安装路由
+Vue.use(VueRouter)
+
+//2.1 导入 vue-resource
+import VueResource from 'vue-resource'
+//2.1 安装 vue-resource
+Vue.use(VueResource)
+
 //导入 MUI样式
 import '../lib/mui/css/mui.min.css'
+import '../lib/mui/css/icons-extra.css'
+import '../lib/mui/fonts/mui-icons-extra.ttf'
 
 //按需导入 Mint-ui 中的组件
-import { Header } from 'mint-ui'
+import { Header,Swipe, SwipeItem } from 'mint-ui'
 import 'mint-ui/lib/style.css'
-
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+
+//1.3导入自己的router.js路由模块
+import router from './router.js'
+
+
 
 
 //导入根组件
@@ -16,5 +35,6 @@ import app from './App.vue'
 
 var vm = new Vue({
     el:'#app',
-    render: c => c(app)
+    render: c => c(app),
+    router  //1.4 挂在路由对象到vm实例对象上
 })
--- src/router.js
+++ src/router.js
@@ -1,12 +1,21 @@
 import VueRouter from 'vue-router'
 
-// 导入路由组件
-
+// 导入对应的路由组件
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShopContainer from './components/tabbar/ShopContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
 
 // 创建路由对象
 const router = new VueRouter({
-  routes: [
-     ],
+  routes: [//配置路由对象
+    {path:'/',redirect:'/home'},
+    {path:'/home',component:HomeContainer},
+    {path:'/member',component:MemberContainer},
+    {path:'/shopcar',component:ShopContainer},
+    {path:'/search',component:SearchContainer},
+  ],
+  linkActiveClass:'mui-active' // 覆盖默认路由高亮的类 ，默认的类叫做router-link-active
 
 })
 
